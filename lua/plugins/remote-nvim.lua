235b2d869389d025a6c1a8a7983f74bd85d87460
Eduard Smet <contact@celarye.dev>
2025-04-28 23:51:08 +0200

    Added remote nvim support

---
+++ lua/plugins/remote-nvim.lua
@@ -0,0 +1,23 @@
+return {
+  "amitds1997/remote-nvim.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  opts = {
+    client_callback = function(port, workspace_config)
+      local cmd = ("wezterm cli --set-window-title --pane-id $(wezterm cli spawn --new-window nvim --server localhost:%s --remote-ui) %s"):format(
+        port,
+        ("'Remote: %s'"):format(workspace_config.host)
+      )
+      vim.fn.jobstart(cmd, {
+        detach = true,
+        on_exit = function(job_id, exit_code, event_type)
+          -- This function will be called when the job exits
+          print("Client", job_id, "exited with code", exit_code, "Event type:", event_type)
+        end,
+      })
+    end,
+  },
+}
